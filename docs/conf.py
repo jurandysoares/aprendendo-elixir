47f762f4ef7ee1a7496dd6d80014ebd5e5e35b4c
Jurandy Soares <jurandy.soares@gmail.com>
2023-10-04 02:58:17 -0300

    feat: Edição de páginas diretamente no GitHub

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,15 @@ language = 'pt_BR'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_show_sourcelink = False
 html_theme = 'sphinx_rtd_theme'
 html_title = html_short_title = project
 html_static_path = ['_static']
+
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "jurandysoares", # Username
+    "github_repo": "aprendendo-elixir", # Repo name
+    "github_version": "main", # Version
+    "conf_py_path": "/docs/", # Path in the checkout to the docs root
+}
